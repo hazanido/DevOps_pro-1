ff9988039553b9a08cdda9c420bad6175fb75a60
idohazan1 <132258303+idohazan1@users.noreply.github.com>
2023-06-11 15:00:00 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+
+    res.send('hello')
+})
+app.get('/test',(req,res)=>{
+    res.send('test hello')
+})
+
+module.exports = app
