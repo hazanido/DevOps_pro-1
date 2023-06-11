c6f5c44e5ed462a24c3a3314e4c70c371b42ac1e
hazanido <hazanido@gmail.com>
2023-06-11 22:06:53 +0300

    add server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const uri =
   "mongodb+srv://hazanido:ido100200310@cluster0.qdogqdd.mongodb.net/?retryWrites=true&w=majority";
 
